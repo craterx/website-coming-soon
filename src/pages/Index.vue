a5bd06c98c4b9d50cd2bd35070960566a2fc0402
Anoop B <16367697+anoop-b@users.noreply.github.com>
2020-06-17 20:23:36 +0530

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+
+    <g-image alt="craterx logo" class="logo" src="~/craterx.svg" width="500" />
+
+    <h1>Coming Soon</h1>
+<h4> Contact us at : support@craterx.com</h4>
+    <p>
+      Thank you for visiting! we're currently migrating our website.
+      We're working hard on something new and exciting!
+      we can't wait to show it to the world.
+      Thank you for your patience.
+    </p>
+    <h3>check out our : <g-link to='//blog.craterx.com'>Blog</g-link> </h3>
+    
+  </Layout>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: 'CraterX'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+
+.logo{
+  display: block ;
+  justify-self: auto;
+}
+</style>
